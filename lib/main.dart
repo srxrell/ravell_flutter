842d7d4057e3be38febba13525f948a3b5429c35
Serrel Vorne <serrelvorne@gmail.com>
2025-12-01 18:25:14 +0500

    FINAL VERSION 1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'readreels.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(ReadReelsApp());
+void main() {
+  // Оборачиваем в try-catch для отлова ошибок при запуске
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      runApp(const ReadReelsApp());
+    },
+    (error, stackTrace) {
+      // Логируем ошибку
+      print('🚨 CRASH: $error');
+      print('Stack trace: $stackTrace');
+      // Можно отправить в Crashlytics или сохранить в файл
+    },
+  );
 }
